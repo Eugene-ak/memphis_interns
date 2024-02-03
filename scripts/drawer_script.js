de6f00a8604952f866e9125115384a772d1ee216
Eugene-ak <amedioreugene@gmail.com>
2024-02-03 11:06:56 +0000

    Update with main branch

--- scripts/drawer_script.js
+++
@@ -1,8 +0,0 @@
-var mobileMenuDrawer = document.getElementById("mobile-menu-button");
-var navListDrawer = document.getElementById("nav-list-drawer");
-
-document.addEventListener("DOMContentLoaded", () => {
-    mobileMenuDrawer.addEventListener("click", () => {
-        navListDrawer.classList.add("show-nav-list");
-    });
-});
